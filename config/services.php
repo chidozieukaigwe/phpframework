9469762ceedb00e89556f14f4fb5bd97bd2c2823
Chidozie Ukaigwe <chido@Chidozies-Air.home>
2024-10-27 15:24:53 +0000

    env variables integrated into app

--- config/services.php
+++ config/services.php
@@ -7,18 +7,22 @@ use League\Container\Argument\Literal\ArrayArgument;
 use League\Container\Argument\Literal\StringArgument;
 use League\Container\Container;
 use League\Container\ReflectionContainer;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(dirname(__DIR__) . '/.env');
 
 $container = new Container();
-
 
 $container->delegate(new ReflectionContainer(true));
 
 //  parameters for application config 
 
 $routes = include BASE_PATH . '/routes/web.php';
-$appEnv = 'dev';
+$appEnv = $_SERVER['APP_ENV'];
 
 $container->add("APP_ENV", new StringArgument($appEnv));
+
 //  services
 
 $container->add(RouterInterface::class, Router::class);
